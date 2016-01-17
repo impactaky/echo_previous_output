f0418a605fd29caa966477ee2ae05155dd14ef48
taki4416 <taki4416@users.noreply.github.com>
2016-01-17 21:11:49 +0900

    Add option n that chose the nth last command

--- echo_previous_result_from_tmux.zsh
+++ echo_previous_result_from_tmux.zsh
@@ -1,10 +1,18 @@
 function echo_last_result(){
+	PREV_NUM=0
+	while getopts n: opt
+	do
+		case $opt in
+			n) PREV_NUM=$OPTARG
+		esac
+	done
 	CURSOR_LINE=`tmux list-panes -F "#{?pane_active,#{cursor_y},}" | sed '/^$/d'`
-	LINE=(`tmux capture-pane -p -S -100000 | sed -n '/┌─/=' | tail -n 2`)
+	MATCH_LINES=`tmux capture-pane -p -S -100000 | sed -n '/┌─/=' | tail -n $((2+$PREV_NUM))`
+	LINE=(`echo $MATCH_LINES | head -n 2`)
 	if [ $#LINE = 1 ]; then
 		tmux capture-pane -e -p -S -100000 -E $(($CURSOR_LINE-3))
 	else
-		OFFSET=$((CURSOR_LINE-${LINE[2]}))
-		tmux capture-pane -e -p -S $((${LINE[1]}+$OFFSET)) -E $(($CURSOR_LINE-3))
+		OFFSET=$((CURSOR_LINE-`echo $MATCH_LINES | tail -n 1`))
+		tmux capture-pane -e -p -S $((${LINE[1]}+$OFFSET)) -E $((${LINE[2]}+$OFFSET-3))
 	fi
 }

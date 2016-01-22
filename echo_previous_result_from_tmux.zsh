e26333d4c6e66ee616718196d63ded3a0db1ac20
taki4416 <taki4416@users.noreply.github.com>
2016-01-22 23:43:59 +0900

    remove global variables

--- echo_previous_result_from_tmux.zsh
+++ echo_previous_result_from_tmux.zsh
@@ -1,19 +1,19 @@
 function echo_last_result(){
-	local PREV_NUM=0
+	local prev_num=0
 	while getopts n: opt
 	do
 		case $opt in
-			n) PREV_NUM=$OPTARG
+			n) prev_num=$OPTARG
 		esac
 	done
-	local CURSOR_LINE=`tmux list-panes -F "#{?pane_active,#{cursor_y},}" | sed '/^$/d'`
-	MATCH_LINES=`tmux capture-pane -p -S -100000 | sed -n '/┌─/=' | tail -n $((2+$PREV_NUM))`
-	LINE=(`echo $MATCH_LINES | head -n 2`)
-	if [ $#LINE = 1 ]; then
+	local cursor_line=`tmux list-panes -F "#{?pane_active,#{cursor_y},}" | sed '/^$/d'`
+	local -a match_lines
+	match_lines=(`tmux capture-pane -p -S -100000 | sed -n '/┌─/=' | tail -n $((2+$prev_num))`)
+	if [ $#match_lines = 1 ]; then
 		tmux capture-pane -e -p -S -100000 -E $(($CURSOR_LINE-3))
 	else
-		OFFSET=$((CURSOR_LINE-`echo $MATCH_LINES | tail -n 1`))
-		tmux capture-pane -e -p -S $((${LINE[1]}+$OFFSET)) -E $((${LINE[2]}+$OFFSET-3))
+		local offset=$((cursor_line - $match_lines[-1]))
+		tmux capture-pane -e -p -S $((${match_lines[1]}+$offset)) -E $((${match_lines[2]}+$offset-3))
 	fi
 	return 0
 }

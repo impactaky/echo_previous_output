861cbeee0f3704a22abf8eec40eef59ca627b48d
taki4416 <taki4416@users.noreply.github.com>
2016-01-24 18:54:16 +0900

    Add color option

--- echo_previous_result_from_tmux.zsh
+++ echo_previous_result_from_tmux.zsh
@@ -1,19 +1,21 @@
 function echo_last_result(){
 	local prev_num=0
-	while getopts n: opt
+	local capture_option=''
+	while getopts cn: opt
 	do
 		case $opt in
-			n) prev_num=$OPTARG
+			n) prev_num=$OPTARG;;
+			c) capture_option="-e "$capture_option;;
 		esac
 	done
 	local cursor_line=`tmux list-panes -F "#{?pane_active,#{cursor_y},}" | sed '/^$/d'`
 	local -a match_lines
 	match_lines=(`tmux capture-pane -p -S -100000 | sed -n '/┌─/=' | tail -n $((2+$prev_num))`)
 	if [ $#match_lines = 1 ]; then
-		tmux capture-pane -e -p -S -100000 -E $(($CURSOR_LINE-3))
+		tmux capture-pane $capture_option -p -S -100000 -E $(($CURSOR_LINE-3))
 	else
 		local offset=$((cursor_line - $match_lines[-1]))
-		tmux capture-pane -e -p -S $((${match_lines[1]}+$offset)) -E $((${match_lines[2]}+$offset-3))
+		tmux capture-pane $capture_option -p -S $((${match_lines[1]}+$offset)) -E $((${match_lines[2]}+$offset-3))
 	fi
 	return 0
 }

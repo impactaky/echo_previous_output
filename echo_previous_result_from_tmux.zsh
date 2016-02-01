39d490b3375368facfa2ce8d310e331b98f1ea03
taki4416 <taki4416@users.noreply.github.com>
2016-02-02 00:13:54 +0900

    refactoring

--- echo_previous_result_from_tmux.zsh
+++ echo_previous_result_from_tmux.zsh
@@ -1,45 +1,43 @@
 function echo_last_result(){
-	local prev_num=1
-	local capture_option=''
-	local ignore_blank_result=0
-	while getopts cn:sl: opt; do
-		case $opt in
-			n)
-				expr $OPTARG + 0 >/dev/null 2>&1
-				if [ $? -ne 0 ]; then
-					echo "Error: n option has invalid value" 1>&2
-					return 1
-				fi
-				prev_num=$OPTARG;;
-			c) capture_option="-e "$capture_option;;
-			s) ignore_blank_result=1
-		esac
-	done
-	local cursor_line=`tmux list-panes -F "#{?pane_active,#{cursor_y},}" | sed '/^$/d'`
-	local -a match_lines
-	match_lines=(`tmux capture-pane -p -S -100000 | sed -n '/└─/='`)
-	if [ $prev_num -ge $#match_lines ]; then
-		echo "Error: n option value out of range" 1>&2
-		return 1
-	fi
+    local prev_num=1
+    while getopts cn:sl: opt; do
+        case $opt in
+            n)  expr $OPTARG + 0 >/dev/null 2>&1
+                if [ $? -ne 0 ]; then
+                    echo "Error: n option has invalid value" 1>&2
+                    return 1
+                fi
+                prev_num=$OPTARG;;
+            c)  local capture_option="-e";;
+            s)  local ignore_blank_result=1;;
+        esac
+    done
+    local -a match_lines
+    match_lines=(`tmux capture-pane -p -S -100000 | sed -n '/└─/='`)
 
-	if [ $ignore_blank_result = 1 ]; then
-		local i=-1
-		local j=$(($match_lines[$i]-$match_lines[$i-1]==2?0:1))
-		local inv_match_num=`expr -$#match_lines`
-		while [ $j -lt $prev_num ]; do 
-			i=$(($i-1))
-			if [ $i -le $inv_match_num ]; then
-				echo "Error: n option value out of range" 1>&2
-				return 1
-			elif [ $((${match_lines[$i]}-$match_lines[$i-1])) -ne 2 ]; then
-				j=$(($j+1))
-			fi
-		done
-	else
-		local i=-$prev_num
-	fi
-	local offset=$((cursor_line - $match_lines[-1]))
-	tmux capture-pane $capture_option -p -S $(($match_lines[$i-1]+$offset)) -E $(($match_lines[$i]+$offset-3))
-	return 0
+    if [ $ignore_blank_result ]; then
+        local i=-1
+        local j=$(($match_lines[$i]-$match_lines[$i-1]==2?0:1))
+        local inv_match_num=`expr -$#match_lines`
+        while [ $j -lt $prev_num ]; do 
+            i=$(($i-1))
+            if [ $i -le $inv_match_num ]; then
+                echo "Error: n option value out of range" 1>&2
+                return 1
+            elif [ $((${match_lines[$i]}-$match_lines[$i-1])) -ne 2 ]; then
+                j=$(($j+1))
+            fi
+        done
+    else
+        if [ $prev_num -ge $#match_lines ]; then
+            echo "Error: n option value out of range" 1>&2
+            return 1
+        fi
+        local i=-$prev_num
+    fi
+
+    local cursor_line=`tmux list-panes -F "#{?pane_active,#{cursor_y},}" | sed '/^$/d'`
+    local offset=$((cursor_line - $match_lines[-1]))
+    tmux capture-pane $capture_option -p -S $(($match_lines[$i-1]+$offset)) -E $(($match_lines[$i]+$offset-3))
+    return 0
 }

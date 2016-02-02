3bfac81633ca42520edcf9ccc09beac8e5c39745
taki4416 <taki4416@users.noreply.github.com>
2016-02-03 00:58:57 +0900

    Work well in the case of previous command has no output

--- echo_previous_result_from_tmux.zsh
+++ echo_previous_result_from_tmux.zsh
@@ -1,3 +1,6 @@
+PromptHeadPattern='^┌─'
+PromptTailPattern='^└─'
+
 function echo_last_result(){
     local prev_num=1
     while getopts cn:sl: opt; do
@@ -13,32 +16,37 @@ function echo_last_result(){
 			*)  return 2
         esac
     done
-    local -a match_lines
-    match_lines=(`tmux capture-pane -p -S -100000 | sed -n '/└─/='`)
+    local -a match_head_lines
+    local -a match_tail_lines
+    match_head_lines=(`tmux capture-pane -p -S -100000 | sed -n '/'$PromptHeadPattern'/='`)
+    match_tail_lines=(`tmux capture-pane -p -S -100000 | sed -n '/'$PromptTailPattern'/='`)
 
     if [ $ignore_blank_result ]; then
         local i=-1
-        local j=$(($match_lines[$i]-$match_lines[$i-1]==2?0:1))
-        local inv_match_num=`expr -$#match_lines`
+        local j=0
+        local inv_match_num=`expr -$#match_tail_lines`
         while [ $j -lt $prev_num ]; do 
             i=$(($i-1))
             if [ $i -le $inv_match_num ]; then
                 echo "Error: n option value out of range" 1>&2
                 return 1
-            elif [ $((${match_lines[$i]}-$match_lines[$i-1])) -ne 2 ]; then
+            elif [ $(($match_tail_lines[$i]-$match_head_lines[$i+1])) -ne -1 ]; then
                 j=$(($j+1))
             fi
         done
     else
-        if [ $prev_num -ge $#match_lines ]; then
+        if [ $prev_num -ge $#match_tail_lines ]; then
             echo "Error: n option value out of range" 1>&2
             return 1
         fi
-        local i=-$prev_num
+		local i=$((-$prev_num-1))
+		if [ $(($match_tail_lines[$i]-$match_head_lines[$i+1])) = -1 ]; then
+			return 0
+		fi
     fi
 
     local cursor_line=`tmux list-panes -F "#{?pane_active,#{cursor_y},}" | sed '/^$/d'`
-    local offset=$((cursor_line - $match_lines[-1]))
-    tmux capture-pane $capture_option -p -S $(($match_lines[$i-1]+$offset)) -E $(($match_lines[$i]+$offset-3))
+    local offset=$((cursor_line - $match_tail_lines[-1]))
+    tmux capture-pane $capture_option -p -S $(($match_tail_lines[$i]+$offset)) -E $(($match_head_lines[$i+1]+$offset-2))
     return 0
 }

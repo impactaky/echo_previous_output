780d54055f05c0710b282aab5220544c533a78dc
taki4416 <taki4416@users.noreply.github.com>
2017-08-23 14:04:45 +0900

    Refactoring

--- echo_previous_output.zsh
+++ echo_previous_output.zsh
@@ -9,33 +9,33 @@ function echo_previous_output(){
 		echo 'tmux is not running'
 		return 0xf
 	fi
-    local prev_num=1
+	local prev_num=1
 	local blank_result=0
-    while getopts bn: opt; do
-        case $opt in
-            n)  expr $OPTARG + 0 >/dev/null 2>&1
-                if [ $? -ne 0 ]; then
-                    echo "Error: n option has invalid value" 1>&2
-                    return 3
-                fi
-                prev_num=$OPTARG;;
-            b)  blank_result=1;;
+	while getopts bn: opt; do
+		case $opt in
+		n)  expr $OPTARG + 0 >/dev/null 2>&1
+			if [ $? -ne 0 ]; then
+			echo "Error: n option has invalid value" 1>&2
+			return 3
+			fi
+			prev_num=$OPTARG;;
+		b)  blank_result=1;;
 			*)  return 2
-        esac
-    done
+		esac
+	done
 	local -a buffer
 	buffer=("${(@f)$(tmux capture-pane -epJ -S -$SearchLines | sed '/'$Prompt2Pattern'/d')}")
-    local -a match_cmd_lines
+	local -a match_cmd_lines
 	match_cmd_lines=(`print -l $buffer | sed -n '/'$PromptCmdLinePattern'/='`)
 
-    if [ $blank_result -eq 0 ]; then
+	if [ $blank_result -eq 0 ]; then
 		local i=1
 		while [ $i -ne $#match_cmd_lines ]; do
-            if [ $(($match_cmd_lines[-$i]-$match_cmd_lines[-$i-1])) -eq $PromptLines ]; then
+			if [ $(($match_cmd_lines[-$i]-$match_cmd_lines[-$i-1])) -eq $PromptLines ]; then
 				local delete_num=$match_cmd_lines[-$i-1]
 				buffer=("${(@)buffer[1,$delete_num-1]}" "${(@)buffer[$delete_num+$PromptLines,$#buffer]}")
 			fi
-            i=$(($i+1))
+			i=$(($i+1))
 		done
 		match_cmd_lines=(`print -l $buffer | sed -n '/'$PromptCmdLinePattern'/='`)
 	fi
@@ -50,5 +50,5 @@ function echo_previous_output(){
 	fi
 
 	print -l $buffer | sed -n "$(($match_cmd_lines[$i]+1)),$(($match_cmd_lines[$i+1]-$PromptLines))p"
-    return 0
+	return 0
 }
